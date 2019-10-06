0f1adda8287973217aaf1f7152a13a706812fe0b
lgdillin <mailgd9@gmail.com>
2019-10-06 18:49:32 -0500

    started

---
+++ code/bisect.m
@@ -0,0 +1,58 @@
+
+
+% Bisection
+function [l, r, nf] = bisect(fname, a, b, tol)
+    % Function to find the zero of a root bracketing
+    % interval for some function fname between a and b
+    % returns a bracketing interval which closely approximates
+    % the exact answer
+    %
+    % inputs:
+    % fname: function to evaluate
+    % a: left side of interval
+    % b: right side of interval
+    % tol: tolerance of convergence
+    %
+    % returns:
+    % l: left side of the converged bracketing interval
+    % r: right side of the converged bracketing interval
+    % nf: the total number of function evaluations of fname
+    
+    if(b > a)
+        error('bad interval')
+    end
+    
+    % Evaluate the function at the two endpoints
+    fa = feval(fname, a);
+    fb = feval(fname, b);
+    if(sign(fa) == sign(fb))
+        error('Not a root bracketing interval')
+    end
+    
+    nf = 0;
+    l = a;
+    r = b;
+    while(abs(l - r) > tol)
+        % Bisect the interval and evaluate
+        p0 = (a + b) / 2;
+        fp = feval(fname, p0);
+        
+        % Perhaps we found the zero exactly
+        if(fp == 0)
+            l = a;
+            r = b;
+            nf = nf + 1;
+            return
+        end
+        
+        if(sign(fp) ~= sign(fa))
+            a = p0;
+        else
+            b = p0;
+        end
+        nf += 1;
+    end
+    l = a;
+    r = b;
+    return;
+end
